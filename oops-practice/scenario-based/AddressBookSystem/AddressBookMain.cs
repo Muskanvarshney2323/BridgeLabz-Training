fa96b7ea37e62619c13986e17de5f261741babda
muskan <varshneymuskan077@gmail.com>
2026-01-14 15:50:58 +0530

    updating uc-2

--- oops-practice/scenario-based/AddressBookSystem/AddressBookMain.cs
+++ oops-practice/scenario-based/AddressBookSystem/AddressBookMain.cs
@@ -6,9 +6,32 @@ class AddressBookMain
         Console.WriteLine("Welcome to Address Book Program");
 
         IAddressBookUtility utility = new AddressBookUtility();
-        ContactPerson person = utility.GetContactDetails();
-
         IAddressBook addressBook = new AddressBook();
-        addressBook.AddContactPerson(person);
+        bool exit = false;
+        while(!exit)
+        {
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Add Contact Person");
+            Console.WriteLine("2. Exit");
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch(choice)
+            {
+                case 1:
+                    ContactPerson person = utility.GetContactDetails();
+                    addressBook.AddContactPerson(person);
+                    utility.ShowMessage("Contact Person Added Successfully");
+                    break;
+                case 2:
+                    exit = true;
+                    utility.ShowMessage("Exiting Address Book Program.");
+                    break;
+                default:
+                    utility.ShowMessage("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+        
     }
 }
