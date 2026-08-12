4a8ca8f085f0868e960a4c421d2af23dda1656dd
muskan <varshneymuskan077@gmail.com>
2026-08-12 22:04:00 +0530

    Implemented Contacts MVC with EF Core and migrations

---
+++ refresher-training/day9/ContactsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace ContactsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId =>
+            !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ refresher-training/day9/ContactsApp/Program.cs
@@ -0,0 +1,33 @@
+using ContactsApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Contact}/{action=Index}/{id?}");
+
+app.Run();
